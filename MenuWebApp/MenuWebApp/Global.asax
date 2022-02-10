40cf6db447d8d052e15c37f1f189691f1c699253
Samim Hossen <banglarcele1122@gmail.com>
2022-02-10 11:03:45 +0600

    Up

---
+++ MenuWebApp/MenuWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MenuWebApp.MvcApplication" Language="C#" %>
---
+++ MenuWebApp/MenuWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MenuWebApp.Startup))]
+namespace MenuWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
